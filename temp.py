944f5127439e6f1242adccfd94a45143137d8767
ChetaNMahipaL <chetanmahipal5@gmail.com>
2024-02-18 18:51:58 +0530

    node2vec

---
+++ temp.py
@@ -0,0 +1,233 @@
+import matplotlib.pyplot as plt
+from math import isclose
+from sklearn.decomposition import PCA
+import os
+import networkx as nx
+import numpy as np
+import pandas as pd
+from stellargraph import StellarGraph, datasets
+from stellargraph.data import EdgeSplitter
+from collections import Counter
+import multiprocessing
+from IPython.display import display, HTML
+from sklearn.model_selection import train_test_split
+
+data_url = "./Dataset/Cit-HepPh.txt"
+df_data_1 = pd.read_csv(data_url, sep='\t', skiprows=4, names=['FromNodeId', 'ToNodeId'], dtype={'FromNodeId': int, 'ToNodeId': int})
+
+
+data_url = "./Dataset/cit-HepPh-dates.txt"
+df_data_2 = pd.read_csv(data_url, sep='\t', skiprows=1, names=['NodeId', 'Date'], dtype={'NodeId': str, 'Date': str})
+df_data_2['Date'] = pd.to_datetime(df_data_2['Date'])
+df_data_2 = df_data_2[~df_data_2['NodeId'].str.startswith('11')]
+df_data_2['NodeId'] = df_data_2['NodeId'].astype(str).str.lstrip('0')
+df_data_2['NodeId'] = df_data_2['NodeId'].astype(int)
+df_data_2 = df_data_2[df_data_2['Date'].dt.year <= 1993]
+
+df_merged = pd.merge(df_data_1, df_data_2, how='inner', left_on='FromNodeId', right_on='NodeId')
+df_merged['Date'] = pd.to_datetime(df_merged['Date'])
+# Filter out rows where 'ToNodeId' is not present in 'NodeId' column of df_data_2
+df_merged = df_merged[df_merged['ToNodeId'].isin(df_data_2['NodeId'])]
+# Rename columns 'from' and 'to' to 'source' and 'target' respectively
+df_merged.rename(columns={'FromNodeId': 'source', 'ToNodeId': 'target'}, inplace=True)
+# unnodes = df_merged['FromNodeId'].unique()
+# i = 0
+# for nodes in unnodes:
+#     i += 1
+# print(i)
+
+graph = StellarGraph(edges=df_merged)
+# print(graph.info())
+
+# Define an edge splitter on the original graph:
+edge_splitter_test = EdgeSplitter(graph)
+
+# Randomly sample a fraction p=0.1 of all positive links, and same number of negative links, from graph, and obtain the
+# reduced graph graph_test with the sampled links removed:
+graph_test, examples_test, labels_test = edge_splitter_test.train_test_split(
+    p=0.1, method="global"
+)
+
+# print(graph_test.info())
+
+# Do the same process to compute a training subset from within the test graph
+edge_splitter_train = EdgeSplitter(graph_test, graph)
+graph_train, examples, labels = edge_splitter_train.train_test_split(
+    p=0.1, method="global"
+)
+(
+    examples_train,
+    examples_model_selection,
+    labels_train,
+    labels_model_selection,
+) = train_test_split(examples, labels, train_size=0.75, test_size=0.25)
+
+# print(graph_train.info())
+
+p = 1.0
+q = 1.0
+dimensions = 128
+num_walks = 10
+walk_length = 80
+window_size = 10
+num_iter = 1
+workers = multiprocessing.cpu_count()
+
+from stellargraph.data import BiasedRandomWalk
+from gensim.models import Word2Vec
+
+
+def node2vec_embedding(graph, name):
+    rw = BiasedRandomWalk(graph)
+    walks = rw.run(graph.nodes(), n=num_walks, length=walk_length, p=p, q=q)
+    # print(f"Number of random walks for '{name}': {len(walks)}")
+    # print(f"Sample walks: {walks[:3]}")  # Print some sample walks to inspect
+
+    model = Word2Vec(
+        walks,
+        vector_size=dimensions,
+        window=window_size,
+        min_count=0,
+        sg=1,
+        workers=workers,
+        epochs=num_iter,
+    )
+    
+    print("Word2Vec model training completed.")  # Add a print statement to check if training completes
+
+    def get_embedding(u):
+        return model.wv[u]
+
+    return get_embedding
+
+embedding_train = node2vec_embedding(graph_train, "Train Graph")
+
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import StandardScaler
+
+
+# 1. link embeddings
+def link_examples_to_features(link_examples, transform_node, binary_operator):
+    return [
+        binary_operator(transform_node(src), transform_node(dst))
+        for src, dst in link_examples
+    ]
+
+
+# 2. training classifier
+def train_link_prediction_model(
+    link_examples, link_labels, get_embedding, binary_operator
+):
+    clf = link_prediction_classifier()
+    link_features = link_examples_to_features(
+        link_examples, get_embedding, binary_operator
+    )
+    clf.fit(link_features, link_labels)
+    return clf
+
+
+def link_prediction_classifier(max_iter=2000):
+    lr_clf = LogisticRegressionCV(Cs=10, cv=10, scoring="roc_auc", max_iter=max_iter)
+    return Pipeline(steps=[("sc", StandardScaler()), ("clf", lr_clf)])
+
+
+# 3. and 4. evaluate classifier
+def evaluate_link_prediction_model(
+    clf, link_examples_test, link_labels_test, get_embedding, binary_operator
+):
+    link_features_test = link_examples_to_features(
+        link_examples_test, get_embedding, binary_operator
+    )
+    score = evaluate_roc_auc(clf, link_features_test, link_labels_test)
+    return score
+
+
+def evaluate_roc_auc(clf, link_features, link_labels):
+    predicted = clf.predict_proba(link_features)
+
+    # check which class corresponds to positive links
+    positive_column = list(clf.classes_).index(1)
+    return roc_auc_score(link_labels, predicted[:, positive_column])
+
+def operator_hadamard(u, v):
+    return u * v
+
+
+def operator_l1(u, v):
+    return np.abs(u - v)
+
+
+def operator_l2(u, v):
+    return (u - v) ** 2
+
+
+def operator_avg(u, v):
+    return (u + v) / 2.0
+
+
+def run_link_prediction(binary_operator):
+    clf = train_link_prediction_model(
+        examples_train, labels_train, embedding_train, binary_operator
+    )
+    score = evaluate_link_prediction_model(
+        clf,
+        examples_model_selection,
+        labels_model_selection,
+        embedding_train,
+        binary_operator,
+    )
+
+    return {
+        "classifier": clf,
+        "binary_operator": binary_operator,
+        "score": score,
+    }
+
+
+binary_operators = [operator_hadamard, operator_l1, operator_l2, operator_avg]
+
+results = [run_link_prediction(op) for op in binary_operators]
+best_result = max(results, key=lambda result: result["score"])
+
+print(f"Best result from '{best_result['binary_operator'].__name__}'")
+
+temp = pd.DataFrame(
+    [(result["binary_operator"].__name__, result["score"]) for result in results],
+    columns=("name", "ROC AUC score"),
+).set_index("name")
+
+print(temp)
+
+
+embedding_test = node2vec_embedding(graph_test, "Test Graph")
+test_score = evaluate_link_prediction_model(
+    best_result["classifier"],
+    examples_test,
+    labels_test,
+    embedding_test,
+    best_result["binary_operator"],
+)
+print(
+    f"ROC AUC score on test set using '{best_result['binary_operator'].__name__}': {test_score}"
+)
+
+# Calculate edge features for test data
+link_features = link_examples_to_features(
+    examples_test, embedding_test, best_result["binary_operator"]
+)
+
+# Learn a projection from 128 dimensions to 2
+pca = PCA(n_components=2)
+X_transformed = pca.fit_transform(link_features)
+
+# plot the 2-dimensional points
+plt.figure(figsize=(16, 12))
+plt.scatter(
+    X_transformed[:, 0],
+    X_transformed[:, 1],
+    c=np.where(labels_test == 1, "b", "r"),
+    alpha=0.5,
+)
+
